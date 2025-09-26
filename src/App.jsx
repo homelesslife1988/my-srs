3aa9241e2e708c01c62a241b87e246737d0bf04c
homelesslife1988 <homelesslife1988@gmail.com>
2025-09-26 17:31:01 +0000

    Update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Decks from "./pages/Decks";
 import Review from "./pages/Review";
 import Navbar from "./components/Navbar";
 
+// Add your GitHub Pages repo name as basename
+const repoName = "/my-srs";
+
 export default function App() {
   return (
-    <Router>
+    <Router basename={repoName}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
