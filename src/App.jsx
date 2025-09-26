fe5c4a4fce1c89d984e0a0b37485f3fcbe16695c
homelesslife1988 <homelesslife1988@gmail.com>
2025-09-26 18:55:39 +0000

    Fix home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Decks from "./pages/Decks";
 import Review from "./pages/Review";
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <Router basename="/my-srs"> {/* Important for GitHub Pages */}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Decks />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/decks" element={<Decks />} />
         <Route path="/review/:deckId" element={<Review />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
